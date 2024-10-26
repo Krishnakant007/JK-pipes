850e93410076bb88fe8cfed4a4c5e555f23cdc91
Krishnakant chavan <70923432+Krishnakant007@users.noreply.github.com>
2024-10-26 16:15:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -40,47 +40,63 @@
 // <Route path="/" element={<CompanyInfo />} />
 
 
+
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 import Header from "./components/Header";
 import HeaderText from "./components/HeaderText";
 
 import Footer from './components/Footer';
-import ReachUs from './components/ReachUs';
-import CompanyInfo from './components/CompanyInfo';
-import ContactForm from './components/ContactForm';
 import Carousel from './components/Carousel';
+import CompanyInfo from './components/CompanyInfo';
 import ProductCategories from './components/ProductCategories';
+import ContactForm from './components/ContactForm';
+import ReachUs from './components/ReachUs';
+import RatingCarousel from './components/RatingCarousel';
 import SWRProductScreen from './screens/SWRProductScreen';
 import HDPEProductScreen from './screens/HDPEProductScreen';
 import PVCProductScreen from './screens/PVCProductScreen';
 import HDPECoilScreen from './screens/HDPECoilScreen';
 import UPVCProductScreen from './screens/UPVCProductScreen';
-import RatingCarousel from './components/RatingCarousel';
 import DetailScreen from './details/DetailScreen';
 
 function App() {
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={
+  const [activeSection, setActiveSection] = useState('home');
+
+  const renderContent = () => {
+    switch (activeSection) {
+      case 'products':
+        return <ProductCategories />;
+      case 'contact':
+        return <ContactForm />;
+      case 'About':
+        return <ReachUs />;
+      default:
+        return (
           <>
             <Carousel />
-            <HeaderText/>
+  
+             <HeaderText/>
             <CompanyInfo />
-            <ProductCategories />
-            <RatingCarousel/>
-            <ContactForm />
-            <ReachUs />
+            <ProductCategories/>
+            <RatingCarousel />
           </>
-        } />
+        );
+    }
+  };
+
+  return (
+    <Router>
+      <Header setActiveSection={setActiveSection} />
+      <Routes>
+        <Route path="/" element={renderContent()} />
         <Route path="/swr" element={<SWRProductScreen />} />
         <Route path="/hdpe" element={<HDPEProductScreen />} />
         <Route path="/pvc" element={<PVCProductScreen />} />
         <Route path="/hdpecoil" element={<HDPECoilScreen />} />
         <Route path="/upvc" element={<UPVCProductScreen />} />
-       
-            <Route path="/details" element={<DetailScreen />} />
+        <Route path="/details" element={<DetailScreen />} />
       </Routes>
       <Footer />
     </Router>
